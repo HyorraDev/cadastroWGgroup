2860f49b11720b9a916979926eeca6ce86dda4c9
Hyorra.cod <133731447+HyorraDev@users.noreply.github.com>
2023-05-19 22:48:28 -0300

    commite inicial
    primeiro commit

---
+++ css2/mobile-navbar.js
@@ -0,0 +1,11 @@
+
+<div id="b_footerItems"><span>© 2023 Microsoft</span><ul><li>
+    <div id="mcp_footer" data-mcp-url="https:/" class="mcp_footer">
+        <span>Gerenciar Preferências de Cookies</span>
+        </div></li>
+        <li><a id="sb_privacy" target="_blank" href="htps:/">Privacidade e Cookies</a></li>
+        <li><a id="sb_legal" target="_blank" href="https:">Legal</a></li>
+        <li><a id="sb_advertise" target="_blank" href="https:">Anunciar</a></li>
+        <li><a id="sb_adinfo" target="_blank" href="https:">Sobre os nossos anúncios</a></li>
+        <li><a id="sb_help" target="_blank" href="https: ">Ajuda</a></li>
+        <li><a role="button" id="sb_feedback" href="#" h="##" class="feedback-binded">Comentários</a></li></ul></div>
